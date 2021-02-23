6f79551d5add6bbf0461d932ea5d286c7ca0fce5
jiajie <j13438323708@163.com>
2021-02-23 09:13:01 +0800

    智慧党建

--- src/components/Moudle_xs/OveSituation/OveSituation.vue
+++ src/components/Moudle_xs/OveSituation/OveSituation.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-02-20 13:55:52
- * @LastEditTime: 2021-02-22 18:03:09
+ * @LastEditTime: 2021-02-23 09:12:06
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \XS\src\components\Moudle_xs\OveSituation.vue
@@ -14,7 +14,7 @@
     id="OveSituation"
   >
     <div class="left">
-      <com-statistics></com-statistics>
+      
     </div>
     <div class="right">
 
@@ -22,13 +22,10 @@
   </div>
 </template>
 <script>
-import comstatistics from "./left/ComStatistics.vue";
+
 
 export default {
   name:"OveSituation",
-  components: {
-    "com-statistics": comstatistics,
-  },
   setup() {
     return {
       
